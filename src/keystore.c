41986cf2dac10f352cd8d1b5fe5e87a3c7e9dda3
Saleem Ansari <tuxdna@gmail.com>
2012-11-29 18:17:44 +0530

    added keystore

--- src/keystore.c
+++ src/keystore.c
@@ -1,19 +1,19 @@
-#include <db.h>
+#include "keystore.h"
 
 static int initialized = 0;
 static DB *dbp;
 static int ret;
 static DBT bdb_key, bdb_data;
 
-void bdb_initialize() {
+void keystore_initialize(const char *dbfile, const char *dbname) {
   if(!initialized) {
-
+    
     if ((ret = db_create(&dbp, NULL, 0)) != 0) {
       fprintf(stderr, "db_create: %s\n", db_strerror(ret));
       exit (1);
     }
-    if ((ret = dbp->open(dbp, NULL, DATABASE, NULL, DB_BTREE, DB_CREATE, 0664)) != 0) {
-      dbp->err(dbp, ret, "%s", DATABASE);
+    if ((ret = dbp->open(dbp, NULL, dbfile, dbname, DB_BTREE, DB_CREATE, 0664)) != 0) {
+      dbp->err(dbp, ret, "%s:%s", dbfile, dbname);
       fprintf(stderr, "db_initialize failed: %s\n", db_strerror(ret));
       exit (1);
     }
@@ -22,10 +22,16 @@ void bdb_initialize() {
   }
 }
 
-/* returns 
-   0 on FAILURE
-   1 on SUCCESS */
-int put_key_value(void *k, int ksize, void *v, int vsize) {
+void keystore_destroy() {
+  if(initialized) {
+    int t_ret = ret;
+    if ((t_ret = dbp->close(dbp, 0)) != 0 && ret == 0) {ret = t_ret; }
+    initialized = 0;
+  }
+}
+
+/* returns: 0 on FAILURE 1, on SUCCESS */
+int keystore_put(void *k, int ksize, void *v, int vsize) {
   bdb_key.data = k;
   bdb_key.size = ksize;
   bdb_data.data = v;
@@ -40,11 +46,8 @@ int put_key_value(void *k, int ksize, void *v, int vsize) {
   }
 }
 
-/* returns 
-   0 on FAILURE
-   1 on SUCCESS */
-void get_key_value(void *k, int ksize, void **p_v, int *p_vsize) {
-
+/* returns: 0 on FAILURE 1, on SUCCESS */
+int keystore_get(void *k, int ksize, void **p_v, int *p_vsize) {
   if ((ret = dbp->get(dbp, NULL, &bdb_key, &bdb_data, 0)) == 0) {
     *p_v = bdb_data.data;
     *p_vsize = bdb_data.size;
@@ -54,12 +57,5 @@ void get_key_value(void *k, int ksize, void **p_v, int *p_vsize) {
     dbp->err(dbp, ret, "DB->get");
     return 0;
   }
-  
 }
 
-void bdb_finalize() {
-  if(initialized) {
-    db_close(&dbp, 0);
-    initialized = 0;
-  }
-}
