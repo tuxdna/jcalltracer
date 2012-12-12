c06a4b84afe3326629c182d5984ad5f8011366ce
Saleem Ansari <tuxdna@gmail.com>
2012-12-13 01:02:30 +0530

    Now the tree comes out nicely!

---
+++ bin/tree.rb
@@ -0,0 +1,77 @@
+#!/usr/bin/ruby
+
+# tree node:
+# { 
+#   key: node_key,
+#   metadata: [class_name, method_name, method_signature],
+#   order: sort_order,
+#   children: [],
+# }
+
+
+def print_tree(node, level)
+  return if node.nil?
+  cnodes = node[:children]
+  return if cnodes.length == 0
+
+  puts "  "*level+node[:metadata].join(", ")
+  cnodes.each do |child_node|
+    print_tree(child_node, level+1)
+  end
+end
+
+def print_tree(node, level)
+  return if node.nil?
+  cnodes = node[:children]
+  return if cnodes.length == 0
+
+  puts "  "*level+node[:metadata].join(", ")
+  cnodes.each do |child_node|
+    print_tree(child_node, level+1)
+  end
+end
+
+threads = {}
+
+subtrees = {}
+
+File.open("threads.out").readlines.map {|l| l.chomp}.each do |tid|
+
+  thread_subtree = []
+  thread_node = {
+    :key => tid,
+    :metadata => [],
+    :order => 1,
+    :children => thread_subtree
+  }
+  threads[tid] = thread_node
+  subtrees[tid] = thread_subtree
+end
+
+File.open("edges.out").readlines.each do |line|
+  nkey, pkey, order, mn, ms, cn = line.chomp.split
+  order = order.to_i
+
+  this_node = {
+    :key => nkey,
+    :metadata => [cn, mn, ms],
+    :order => order,
+    :children => []
+  }
+
+  parent_tree_nodes = subtrees[pkey]
+  if parent_tree_nodes.nil?
+    parent_tree_nodes = []
+    subtrees[pkey] = parent_tree_nodes
+  end
+
+  parent_tree_nodes.push(this_node)
+
+  subtrees[nkey] = this_node[:children]
+end
+
+
+threads.each do |k,v|
+  puts "thread: #{k}"
+  print_tree(v, 0)
+end
--- test-src/com/test/HelloWorld.java
+++ test-src/com/test/HelloWorld.java
@@ -1,15 +1,28 @@
 package com.test;
 
 public class HelloWorld {
+    private void method4() {
+	System.out.println("inside method4()");
+	this.method3();
+    }
+
     public void method1() {
+	System.out.println("inside method1()");
+	this.method3();
 	return;
     }
+
     public void method2() {
+	System.out.println("inside method2()");
+	this.method4();
 	return;
     }
+
     public void method3() {
+	System.out.println("inside method3()");
 	return;
     }
+
     public static void main(String[] args) {
 	System.out.println("Hello World!\n");
 	String s = new String("This is cool");
